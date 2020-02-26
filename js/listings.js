0874e22a314f688aa019e717d271e8c1e3520085
Ethan Zack <ethanzack2021@u.northwestern.edu>
2020-02-26 16:56:30 -0600

    listings

--- js/listings.js
+++ js/listings.js
@@ -1,5 +1,5 @@
 class Listing {
-    constructor(title, email, address, zip, phone, housing_type, notes = "") {
+    constructor(title, email, address, zip, phone, housing_type, notes = "", price= 0) {
         this.title = title;
         this.email = email;
         this.address = address;
@@ -7,6 +7,7 @@ class Listing {
         this.phone = phone;
         this.housing_type = housing_type;
         this.notes = notes;
+        this.price = price
     }
     // TODO: add phone to card
     /**
@@ -14,21 +15,35 @@ class Listing {
      */
     generate_card() {
         return `<div class="card listing-card">
-                <div class="card-header">${this.housing_type}
+                <div class="card-header">${this.housing_type}: ${this.title}<div class="float-right price" style = "font-weight = 2, color= green">$${this.price}</div>
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title">${this.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${this.address}, ${this.zip}</h6>
-                    <p class="card-text">${this.notes}</p>
-                    <button class="btn"><a href="mailto:${this.email}">${this.email}</a></button>
-                </div>
+
+                  <table class="card-table table table-borderless" style="border: 0">
+                    <thead >
+                      <tr>
+                        <th scope="col">Description</th>
+                        <th scope="col">Info</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>${this.notes}</td>
+                        <td>
+                          <p class = "card-text ">${this.address}, ${this.zip}</h6>
+                          <br />
+                          <a href="">${this.phone}</a><br />
+                          <a href="mailto:${this.email}">${this.email}</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
             </div>`
     }
-    
+
     /**
      * Converts self to json string
      */
-    convert_to_json() {        
+    convert_to_json() {
         return JSON.stringify(this);
     }
 
@@ -40,11 +55,11 @@ class Listing {
         if (listings_content) {
             listings_content.innerHTML += this.generate_card()
         }
-        
+
         if (geocoder && map) {
             markAddress(geocoder, map, address);
         }
-    
+
         let listings = new Set(JSON.parse(localStorage.getItem('listings')) || [])
         const json = this.convert_to_json();
         if (!listings.has(json)) {
