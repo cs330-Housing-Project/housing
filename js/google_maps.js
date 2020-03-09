5b88b1306ae024e051d8f202ce08e9fe0bd578be
Ethan Zack <ethanzack2021@u.northwestern.edu>
2020-03-09 17:43:45 -0500

    modal

--- js/google_maps.js
+++ js/google_maps.js
@@ -23,35 +23,42 @@ function initializeMap() {
         email = "mortyschapiro@northwestern.edu",
         address = "1922 Sheridan Rd. Evanston, Il",
         zip = "60201", phone = "123-456-7890",
-        housing_type = "House", notes = "Super dope place", price = 1300)).add_listing_to_local_storage();
+        housing_type = "House", notes = "Super dope place", price = 1300,
+        amenities = "Free Wifi, Linens, Hot Water"
+
+      )).add_listing_to_local_storage();
 
     (new Listing(
         title = "A cool house",
         email = "blob@northwestern.edu",
         address = "2420 Campus Dr. Evanston, Il",
         zip = "60201", phone = "123-456-7890",
-        housing_type = "Apartment", notes = "Nice", price = 1200)).add_listing_to_local_storage();
+        housing_type = "Apartment", notes = "Nice", price = 1200,
+        amenities = "Free Wifi, Linens, Hot Water")).add_listing_to_local_storage();
 
     (new Listing(
         title = "Itay's House",
         email = "stuff@northwestern.edu",
         address = "2110 Maple Ave. Evanston, Il",
         zip = "60201", phone = "123-456-7890",
-        housing_type = "House", notes = "8 person house", price = 700)).add_listing_to_local_storage();
+        housing_type = "House", notes = "8 person house", price = 700,
+        amenities = "Free Wifi, Linens, Hot Water")).add_listing_to_local_storage();
 
     (new Listing(
         title = "Ethan's House",
         email = "stuff@northwestern.edu",
         address = "2039 Sherman Ave. Evanston, Il",
         zip = "60201", phone = "333-333-3333",
-        housing_type = "House", notes = "4 person house", price = 800)).add_listing_to_local_storage();
+        housing_type = "House", notes = "4 person house", price = 800,
+        amenities = "Free Wifi, Linens, Hot Water")).add_listing_to_local_storage();
 
     (new Listing(
         title = "A nice sublet",
         email = "stuff@northwestern.edu",
         address = "2212 Noyes St. Evanston, Il",
         zip = "60201", phone = "111-111-1111",
-        housing_type = "Sublet", notes = "8 person house", price = 1000)).add_listing_to_local_storage();
+        housing_type = "Sublet", notes = "8 person house", price = 1000,
+        amenities = "Free Wifi, Linens, Hot Water")).add_listing_to_local_storage();
 }
 
 function markAddress(geocoder, map, address) {
