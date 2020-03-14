cbdb1000df5e00404053cd2b1045bb3029c2a0a3
Ethan Zack <ethanzack2021@u.northwestern.edu>
2020-03-14 11:54:50 -0500

    messaging

--- js/message.js
+++ js/message.js
@@ -2,20 +2,20 @@ let msgArr = [
   ["Hello", "Hi", "Are you looking for a roommate?", "Yes!", "For what address? How many people? What is the price?",
   "5 people at 1234 Sheridan Road. It's $1000", "I will let you know!"],
 
-  ["Hey", "Hey what's up", "Are you looking for a roommate?", "Yes!", "For what address? How many people? What is the price?",
-  "5 people at 1234 Sheridan Road. It's $1000", "I will let you know!"],
+  ["Hey", "Hey what's up", "Nothing much. Just looking for a roommate", "Me too! where would you like to live",
+  "preferably north campus under $1000", "Let's look into that!"],
 
-  ["Hello", "Hi", "Are you looking for a roommate?", "Yes!", "For what address? How many people? What is the price?",
-  "5 people at 1234 Sheridan Road. It's $1000", "I will let you know!"],
+  ["Hi", "Hi", "I am trying to sublet in the spring", "I can offer you a sublet for $600",
+  "I'll get back to you soon", "Thank you!"],
 
-  ["Hello", "Hi", "Are you looking for a roommate?", "Yes!", "For what address? How many people? What is the price?",
-  "5 people at 1234 Sheridan Road. It's $1000", "I will let you know!"],
+  ["Hello", "Hi", "I'm looking for a subletter", "I can be your subletter in the suummer!",
+  "Really, awesome. It's $900 per month", "I'll get back to you ASAP'"],
 
-  ["Hello", "Hi", "Are you looking for a roommate?", "Yes!", "For what address? How many people? What is the price?",
-  "5 people at 1234 Sheridan Road. It's $1000", "I will let you know!"],
+  ["Hey", "Hey", "We should contact the landlord soon", "I agree, we want to secure our lease", "Can you message her",
+  "Sure, I will ASAP", "Sounds great"],
 
-  ["Hello", "Hi", "Are you looking for a roommate?", "Yes!", "For what address? How many people? What is the price?",
-  "5 people at 1234 Sheridan Road. It's $1000", "I will let you know!"],
+  ["Hello", "Hey", "I'm looking for  a roommate this fall", "I can be your roommate", "Really? cool!",
+  "Yep! I'll send you the details soon", "Sounds good!"],
 
   ["Hello", "Hi", "Are you looking for a roommate?", "Yes!", "For what address? How many people? What is the price?",
   "5 people at 1234 Sheridan Road. It's $1000", "I will let you know!"]
